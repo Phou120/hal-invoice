bbeaab30cbd825a9da316ea38467f81e39c31c0d
paovang <vangpaovang@gmail.com>
2023-07-21 10:18:20 +0700

    update

--- app/Services/CalculateService.php
+++ app/Services/CalculateService.php
@@ -19,7 +19,7 @@ class CalculateService
     /** calculate quotation */
     public function calculateTotal($request, $sumSubTotal, $id)
     {
-        /** Calculate **/
+        /** Calculate 1 **/
         $sumTotalTax = $sumSubTotal * $request['tax'] / 100;
         $sumTotalDiscount = $sumSubTotal * $request['discount'] / 100;
         $sumTotal = ($sumSubTotal - $sumTotalDiscount) + $sumTotalTax;
