82fe19cb819b381b3d64fe7b86baa8cc799512ae
Phou120 <lingkp120@gmail.com>
2023-08-10 12:13:03 +0700

    update

--- app/Services/User/UserProfileService.php
+++ app/Services/User/UserProfileService.php
@@ -16,14 +16,32 @@ class UserProfileService
         $user = Auth::user('api');
 
         $listUser = User::select(
-            'users.name', 'users.email', 'users.password', 'users.created_at', 'users.updated_at',
+            'users.*',
             DB::raw("CONCAT('" . config('services.master_path.user_profile') . "', users.profile) AS profile_url")
         )
-        ->where('id', $user->id)
+        ->leftJoin('role_user', 'users.id', '=', 'role_user.user_id')
+        ->leftJoin('roles', 'role_user.role_id', '=', 'roles.id')
+        ->leftJoin('permission_role', 'roles.id', '=', 'permission_role.role_id')
+        ->leftJoin('permissions', 'permission_role.permission_id', '=', 'permissions.id')
+        ->where('users.id', $user->id)
         ->first();
 
+        $roleUser = $listUser->roles->pluck('name'); // Assuming the role name is stored in the 'name' column
+        $permissionRole = $listUser->roles->flatMap(function ($role) {
+            return $role->permissions->pluck('name');
+        });
+
         return response()->json([
-            'user' => $listUser
+            'user' => [
+                'name' => $listUser->name,
+                'email' => $listUser->email,
+                'profile_url' => $listUser->profile_url,
+                'tel' => $listUser->tel,
+                'created_at' => $listUser->created_at,
+                'updated_at' => $listUser->updated_at,
+                'roleUser' => $roleUser,
+                'permissionRole' => $permissionRole,
+            ]
         ]);
     }
 }
